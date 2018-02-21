6e9bdbc3629a3ead06168e54013fb83ab57de594
programbench\programbench <thanasakis@gmail.com>
2018-02-21 23:33:09 +0200

    first attempt to finish project

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,45 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private static final int MINIMUM_TEXT_SIZE = 2;
+    @BindView(R.id.image_iv)
+    ImageView ingredientsIv;
+    @BindView(R.id.name_label)
+    TextView nameLabel;
+    @BindView(R.id.also_known_tv)
+    TextView alsoKnownTv;
+    @BindView(R.id.origin_tv)
+    TextView originTv;
+    @BindView(R.id.ingredients_tv)
+    TextView ingredientsTv;
+    @BindView(R.id.description_tv)
+    TextView descriptionTv;
+    @BindString(R.string.not_apllicable)
+    String notApplicable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -37,18 +58,15 @@ public class DetailActivity extends AppCompatActivity {
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
         Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Log.v("sandwich", sandwich.toString());
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
+        populateUI(sandwich);
 
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +74,28 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .error(R.mipmap.ic_launcher)
+                .into(ingredientsIv);
 
+        setTitle(sandwich.getMainName());
+        nameLabel.setText(sandwich.getMainName());
+        alsoKnownTv.setText(sandwich.getAlsoKnownAs()
+                .toString()
+                .replace("[", "")
+                .replace("]", ""));
+        if (alsoKnownTv.length() < MINIMUM_TEXT_SIZE) {
+            alsoKnownTv.setText(notApplicable);
+        }
+        ingredientsTv.setText(sandwich.getIngredients().toString()
+                .replace("[", "")
+                .replace("]", ""));
+        originTv.setText(sandwich.getPlaceOfOrigin());
+        if (originTv.length() < MINIMUM_TEXT_SIZE) {
+            originTv.setText(notApplicable);
+        }
+        descriptionTv.setText(sandwich.getDescription());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,57 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        List<String> mAlsoKnownAs = new ArrayList<>();
+        List<String> mIngredients = new ArrayList<>();
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            JSONObject sandwichName = sandwichJson.getJSONObject("name");
+            String mName = sandwichName.getString("mainName");
+            JSONArray sandwichKnownArraysandwichName = sandwichName.getJSONArray("alsoKnownAs");
+            JSONArray sandwichIngredients = sandwichJson.getJSONArray("ingredients");
+            mAlsoKnownAs = convertJSONArrayToList(sandwichKnownArraysandwichName);
+            mIngredients = convertJSONArrayToList(sandwichIngredients);
+            String mDescription = sandwichJson.getString("description");
+            String mPlaceOfOrigin = sandwichJson.getString("placeOfOrigin");
+            String mImage = sandwichJson.getString("image");
+            sandwich = new Sandwich(mName, mAlsoKnownAs, mPlaceOfOrigin, mDescription, mImage, mIngredients);
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("JSONUtils", "Problem parsing JSON object", e);
+        }
+        return sandwich;
+    }
+
+    private static List<String> convertJSONArrayToList(JSONArray jsonArray) {
+        List<String> retList = new ArrayList<String>();
+        try {
+            if (jsonArray != null) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    retList.add(jsonArray.getString(i));
+                }
+            }
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("convertJSONArrayToList", "Problem parsing JSON object", e);
+        }
+        return retList;
     }
 }
